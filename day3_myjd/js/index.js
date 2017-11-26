6d7e0ab7086530e9c21cc8291615146ac4ce1320
unknown <739736709@qq.com>
2017-11-26 21:15:38 +0800

    touchstart等移动端事件以及addeventlistener的dom2事件中的transitionEnd（过渡结束事件）

--- day3_myjd/js/index.js
+++ day3_myjd/js/index.js
@@ -10,7 +10,7 @@ function search() {
 
 	window.onscroll = function() {
 		var top = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
-		console.log(top);
+
 		if(top < height) {
 			var opacity = top / height * 0.85;
 		} else {
@@ -26,7 +26,6 @@ function banner() {
 	var jd_banner = document.getElementsByClassName("jd_banner")[0];
 	var ul1 = jd_banner.children[0];
 	var ul2 = jd_banner.children[1];
-	console.log(ul2);
 	var w = ul1.offsetWidth / 10;
 	//	添加过度
 	var addTransition = function() {
@@ -46,34 +45,78 @@ function banner() {
 
 	//页面加载完毕后自动滚动
 	var index = 1;
-	var square = 1;
 	var timer = setInterval(function() {
 		index++;
-		square++;
 		ul1.style.transition = "all 0.2s";
 		ul1.style.transform = "translateX(" + -index * w + "px)";
-		
 
 	}, 1000)
-	
+
 	//本案例的核心： 过渡结束事件  会在上面的这个结束才会执行 也就是说 当index=9时  ， 先滑动到10张 然后再清除 过渡 瞬间运动到第2张
 	ul1.addEventListener("transitionend", function() {
-		console.log(index);
+
 		if(index >= 9) {
 			index = 1;
 		}
 		ul1.style.transition = "none";
 		ul1.style.transform = "translateX(" + -index * w + "px)";
-		
-		
+
 		for(var i = 0; i < ul2.children.length; i++) {
 			ul2.children[i].className = '';
 		}
 		ul2.children[index - 1].className = 'now';
 
 	})
-	
-	
+	//手机触摸滑动轮播图的事件
+	//1.touchstart
+	var startX = 0;
+	var moveX = 0;
+	var touchX = 0;
+	//必须添加一个isMove参数  不然  的话原地点击时 moveX为上次的值
+	var isMove = false;
+	ul1.addEventListener("touchstart", function(event) {
+		clearInterval(timer);
+		startX = event.touches[0].clientX;
+	});
+
+	ul1.addEventListener("touchmove", function(event) {
+		isMove = true;
+		moveX = event.touches[0].clientX;
+		touchX = moveX - startX;
+		removeTransition();
+		setTranslateX(-index * w + touchX);
+
+	});
+	ul1.addEventListener("touchend", function(event) {
+		console.log(startX);
+		console.log(moveX);
+		console.log(touchX);
+		//isMove必须为 true，  不然 moveX 的值 不会更新 因此数据不准确  ；
+		if((Math.abs(touchX) >= w / 4) && isMove) {
+			if(touchX > 0) {
+				index--;
+			} else {
+				index++;
+			}
+			addTransition();
+			setTranslateX(-index * w);
+		} else {
+			addTransition();
+			setTranslateX(-index * w);
+		}
+		//重新加载轮播图滚动事件
+		startX = 0;
+		moveX = 0;
+		touchX = 0;
+		isMove = false;
+		clearInterval(timer);
+		timer = setInterval(function() {
+			index++;
+			addTransition();
+			setTranslateX(-index * w);
+		}, 1000);
+
+	});
 
 }
 
